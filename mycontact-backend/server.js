f16868f33841f477079eab517c4af18e6aabe729
DebakBhattacharjee <112881738+DebakBhattacharjee@users.noreply.github.com>
2024-07-16 01:47:59 +0530

    commit

--- mycontact-backend/server.js
+++ mycontact-backend/server.js
@@ -1,8 +1,16 @@
-const express = require('express')
-
+const express = require('express');
+const dotenv = require('dotenv').config();
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5001;
+
+
+app.get('/api/contacts', (req, res)=>{
+  res.json({message: "Get all contacts"});
+
+});
+
+
 
 app.listen(port, () =>{
   console.log('Server running on port ${port}');
